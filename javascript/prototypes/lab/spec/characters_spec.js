28422fc1c4c1234f8e94b719ee2e001b3f08c439
Guy Royse <guy@guyroyse.com>
2012-09-07 21:35:24 -0400

    Added prototype lab and completed presentation, deleted stray files

---
+++ javascript/prototypes/lab/spec/characters_spec.js
@@ -0,0 +1,11 @@
+describe("Characters", function() {
+
+  describe("Alice", function() {
+
+    it("has a name", function() {
+      expect(alice.name).toBe('Alice Spellster');
+    });
+
+  });
+
+});
